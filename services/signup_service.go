08f41501bc62869eef25d85aeee109903f3c5f07
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-07-15 11:06:13 -0400

    Add signup service scaffold

---
+++ services/signup_service.go
@@ -0,0 +1,9 @@
+package services
+
+func SignupService(username string, password string) bool {
+	// for now, just always return true
+	if (username == "braeden" && password == "pokerdegen") {
+		return true
+	}
+	return false
+}
