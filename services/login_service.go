f70b2cee01241e9f00c7a8b2bd26ba56b03a7262
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-07-15 18:39:57 -0400

    Fix password comparison logic to work with hashes passwords

--- services/login_service.go
+++ services/login_service.go
@@ -10,7 +10,18 @@ func LoginService(username string, password string) error {
 	if err != nil {
 		return err
 	}
-	hashed, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	_, err = database.FetchUser(db, username, string(hashed))
+	hashedPassword, err := database.FetchPasswordForUser(db, username)
+	if err != nil {
+		return err
+	}
+	err = comparePassword(hashedPassword, password)
+	if err != nil {
+		return err
+	}
+	_, err = database.FetchUser(db, username)
 	return err
+}
+
+func comparePassword(hashedPassword string, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
